6aa61f674668b5642d3079c1dac471522df3fadb
edublasi <eblasi@unifesp.br>
2026-04-17 09:32:01 -0300

    Update 1000.c
    Added header comments with problem details and objectives.

--- 1000.c
+++ 1000.c
@@ -1,3 +1,12 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : Eduardo Abreu
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1000
+Data        : 8/04/2026
+Objetivo    : Imprimir a mensagem "Hello World!".
+Aprendizado : Introdução à linguagem C e suas funções fundamentais.
+-------------------------------------------------------------------------- */
 #include "stdio.h"
 
 int main(){
